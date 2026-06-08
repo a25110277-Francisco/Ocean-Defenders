83d22aa759c48d7773a51f104c7232fe51dbf872
a25110277-Francisco <a25110277@ceti.mx>
2026-06-08 11:50:51 -0600

    Se cambio el color del item a amarillo.

--- include/recolectables/SuministroOxigeno.hpp
+++ include/recolectables/SuministroOxigeno.hpp
@@ -6,7 +6,7 @@
 class SuministroOxigeno : public PowerUp {
 public:
     explicit SuministroOxigeno(Posicion posicionInicial)
-        : PowerUp(posicionInicial, 0.0f, sf::Color(60, 170, 255)), recuperacionOxigeno(30) {}
+        : PowerUp(posicionInicial, 0.0f, sf::Color(60, 190, 255)), recuperacionOxigeno(30) {}
 
     void Activar(Submarino& submarino) override {
         submarino.RegenerarOxigeno(recuperacionOxigeno);
